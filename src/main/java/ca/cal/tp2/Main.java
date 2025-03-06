9b842f7a1a8a4881e5eb375382d27e7570935456
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-03-06 16:01:16 -0500

    TcpServer ajouté et configuration de la BD H2 corrigé

--- src/main/java/ca/cal/tp2/Main.java
+++ src/main/java/ca/cal/tp2/Main.java
@@ -1,6 +1,7 @@
 package ca.cal.tp2;
 
 import ca.cal.tp2.exception.DatabaseException;
+import java.sql.SQLException;
 import ca.cal.tp2.service.LibrarianService;
 import ca.cal.tp2.modele.Book;
 import ca.cal.tp2.modele.CD;
@@ -10,53 +11,29 @@ import ca.cal.tp2.service.BorrowerService;
 import ca.cal.tp2.modele.Borrower;
 
 public class Main {
-    public static void main(String[] args) throws DatabaseException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException, SQLException {
+        TcpServer.createTcpServer();
 //        LibrarianService librarianService = new LibrarianService();
 //        librarianService.addLibraryDocument(
-//            new Book(
-//                "Livre Exemple",
-//                "Auteur",
-//                LocalDate.of(2025, 1, 1),
-//                "Éditeur",
-//                100
-//            )
+//            new Book("Livre Exemple", "Auteur", LocalDate.of(2025, 1, 1), "Éditeur", 100)
 //        );
 //        librarianService.addLibraryDocument(
-//            new CD(
-//                "CD Exemple",
-//                "Artiste",
-//                LocalDate.of(2025, 1, 1),
-//                240,
-//                "Acoustique"
-//            )
+//            new CD("CD Exemple", "Artiste", LocalDate.of(2025, 1, 1), 240, "Acoustique")
 //        );
 //        librarianService.addLibraryDocument(
-//            new DVD(
-//                "DVD Exemple",
-//                "Directeur",
-//                LocalDate.of(2025, 1, 1),
-//                7200,
-//                10
-//            )
+//            new DVD("DVD Exemple", "Directeur", LocalDate.of(2025, 1, 1), 7200, 10)
 //        );
         BorrowerService borrowerService = new BorrowerService();
         try {
             borrowerService.addBorrower(
-                    new Borrower(
-                            "Emprunteur",
-                            "Exemple",
-                            "courriel@exemple.com",
-                            "000-000-0000"
-                    )
+                new Borrower("Emprunteur", "Exemple", "courriel@exemple.com", "000-000-0000")
             );
-        }
-        catch (DatabaseException e) {
+        } catch (DatabaseException e) {
             System.out.println("Erreur avec la base de données: " + e.getMessage());
         }
         try {
             System.out.println(borrowerService.getBorrower(1));
-        }
-        catch (DatabaseException e) {
+        } catch (DatabaseException e) {
             System.out.println("Erreur avec la base de données: " + e.getMessage());
         }
 
